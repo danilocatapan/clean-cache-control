bb3772eb9942df608327f5b1559d437b8356ae57
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-09 16:11:42 -0300

    update requirements

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -44,7 +44,7 @@ describe('LocalSavePurchases', () => {
     jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => { throw new Error() });
     const promise = sut.save();
     expect(cacheStore.insertCallsCount).toBe(0);
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow();
   });
 
 });
